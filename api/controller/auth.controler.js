9e39329cb96c378fed4c7ee72d670507e6ce2be6
ragnarlothbroke12 <siyaram490don@gmail.com>
2026-01-15 16:55:16 +0530

    sigin api created

--- api/controller/auth.controler.js
+++ api/controller/auth.controler.js
@@ -1,6 +1,44 @@
 import User from '../models/User.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+        console.log(email, password);
+        return;
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            next(errorHandler(404, 'User not found'));
+            return;
+        }
+
+        const isMatch =  bcryptjs.compareSync(password, user.password);
+        if (!isMatch) {
+            next(errorHandler(400, 'Invalid credentials'));
+            return;
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        console.log(process.env.JWT_SECRET);
+
+
+        const { password: pwd, ...others } = user._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json({ ...others });
+    } catch (error) {
+        next(error);
+    }
+};
 
 export const signup = async (req, res, next) => {
     
